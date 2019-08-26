f748de144b811553d5c557a1cf1a067fa5797257
Jon Ransom <jransom12@live.com>
2019-08-26 10:50:43 -0500

    Problems 1 and 2
    Problem 1 completed using the SearchForSubstring method using the built in methods Where and Contains. Problem 2 completed using the build in method Distinct.

---
+++ LINQProblems/Problems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Problems
+    {
+        public void SearchForSubString(string search, List<string> words)
+        {
+            var thwords = words.Where(w => w.Contains(search));
+            foreach (var word in thwords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        public void RemoveDuplicates(List<string> names)
+        {
+            var namesOnce = names.Distinct();
+            foreach (var name in namesOnce)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace LINQProblems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Problems problems = new Problems();
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            problems.SearchForSubString("th", words);
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike", };
+            problems.RemoveDuplicates(names);
+
+            Console.ReadLine();
         }
     }
 }
+
